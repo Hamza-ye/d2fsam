307fd300466ba122a8db1079aa782a1a69584d76
Hamza Assada <7amza.it@gmail.com>
2023-03-04 02:55:13 +0300

    unformat sql script

--- d2fsam-support/d2fsam-support-db-migration/src/main/resources/config/liquibase/sql/separate_sequence_generators.sql
+++ d2fsam-support/d2fsam-support-db-migration/src/main/resources/config/liquibase/sql/separate_sequence_generators.sql
@@ -6,28 +6,22 @@ CREATE SEQUENCE if not exists hibernate_sequence
     CACHE 1;
 
 create sequence if not exists datavalueaudit_sequence;
-select setval('datavalueaudit_sequence', max(datavalueauditid))
-FROM data_value_audit;
+select setval('datavalueaudit_sequence', max(datavalueauditid)) FROM data_value_audit;
 
 create sequence if not exists programstageinstance_sequence;
-select setval('programstageinstance_sequence', max(programstageinstanceid))
-FROM program_stage_instance;
+select setval('programstageinstance_sequence', max(programstageinstanceid)) FROM program_stage_instance;
 
 create sequence if not exists trackedentitydatavalueaudit_sequence;
-select setval('trackedentitydatavalueaudit_sequence', max(trackedentitydatavalueauditid))
-FROM tracked_entity_data_value_audit;
+select setval('trackedentitydatavalueaudit_sequence', max(trackedentitydatavalueauditid)) FROM tracked_entity_data_value_audit;
 
 create sequence if not exists trackedentityinstance_sequence;
-select setval('trackedentityinstance_sequence', max(trackedentityinstanceid))
-FROM tracked_entity_instance;
+select setval('trackedentityinstance_sequence', max(trackedentityinstanceid)) FROM tracked_entity_instance;
 
 create sequence if not exists trackedentityinstanceaudit_sequence;
-select setval('trackedentityinstanceaudit_sequence', max(trackedentityinstanceauditid))
-FROM tracked_entity_instance_audit;
+select setval('trackedentityinstanceaudit_sequence', max(trackedentityinstanceauditid)) FROM tracked_entity_instance_audit;
 
 create sequence if not exists programinstance_sequence;
-select setval('programinstance_sequence', max(programinstanceid))
-FROM program_instance;
+select setval('programinstance_sequence', max(programinstanceid)) FROM program_instance;
 
 -- create sequence if not exists deletedobject_sequence;
 -- select setval('deletedobject_sequence', max(deletedobjectid)) FROM deletedobject;
@@ -36,13 +30,10 @@ FROM program_instance;
 -- select setval('reservedvalue_sequence', max(reservedvalueid)) FROM reservedvalue;
 
 create sequence if not exists usermessage_sequence;
-select setval('usermessage_sequence', max(usermessageid))
-FROM user_message;
+select setval('usermessage_sequence', max(usermessageid)) FROM user_message;
 
 create sequence if not exists messageconversation_sequence;
-select setval('messageconversation_sequence', max(messageconversationid))
-FROM message_conversation;
+select setval('messageconversation_sequence', max(messageconversationid)) FROM message_conversation;
 
 create sequence if not exists message_sequence;
-select setval('message_sequence', max(messageid))
-FROM message;
+select setval('message_sequence', max(messageid)) FROM message;
